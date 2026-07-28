356aad31e8754f3dc02a6fb9d70de3fe8426a40c
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-07-29 01:58:01 +0530

    Add Codeforces folder with Java code

---
+++ Codeforces/A/Watermelon_4A.java
@@ -0,0 +1,18 @@
+package Codeforces.A;
+import java.util.Scanner;
+
+public class Watermelon_4A{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int w = sc.nextInt();
+ 
+        if (w > 2 && w % 2 == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+ 
+        sc.close();
+    }
+}
---
+++ Codeforces/README.md
@@ -0,0 +1,11 @@
+# Codeforces Solutions
+
+This repository contains my daily Codeforces problem solutions.
+
+## Language
+- Java
+
+## Goal
+- Solve at least one Codeforces problem every day.
+- Improve problem-solving skills.
+- Maintain consistency on GitHub.
