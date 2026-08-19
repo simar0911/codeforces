369b73ae1878e8b9f848cf4e40310515c42f4d65
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-08-20 02:09:03 +0530

    Add Team 231A solution

---
+++ Codeforces/A/Team_231A.java
@@ -0,0 +1,23 @@
+package Codeforces.A;
+import java.util.*;
+ 
+public class Team_231A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+        int count = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+ 
+            if (a + b + c >= 2) {
+                count++;
+            }
+        }
+ 
+        System.out.println(count);
+    }
+}
