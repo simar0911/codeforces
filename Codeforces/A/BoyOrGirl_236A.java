1a3334bf769d1a8d65794101ae4db5f03a41cd5a
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-08-23 01:39:09 +0530

    Solved problem 236A BoyOrGirl

---
+++ Codeforces/A/BoyOrGirl_236A.java
@@ -0,0 +1,25 @@
+package Codeforces.A;
+import java.util.*;
+public class BoyOrGirl_236A {
+     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.nextLine();
+        
+        HashSet<Character> set = new HashSet<>();
+        
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
+        
+        if (set.size() % 2 == 0) {
+            System.out.println("CHAT WITH HER!");
+        } else {
+            System.out.println("IGNORE HIM!");
+        }
+        sc.close();
+    }
+}
+    
+
