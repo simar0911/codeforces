953c17cfb4ebed39a632f304e13efa8a12cdf6a2
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-08-29 02:04:13 +0530

    Solved problem 266A Stones on the Table

---
+++ Codeforces/A/266A_StonesOntheTable.java
@@ -0,0 +1,21 @@
+package Codeforces.A;
+import java.util.*;
+class StonesOnTheTable {
+        public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        String s = sc.next();
+        int count = 0;
+        
+        for (int i = 1; i < n; i++){
+        if (s.charAt(i) == s.charAt(i - 1)){
+            count++;
+        }
+        }
+        
+        System.out.println(count);
+        sc.close();
+    }
+}
+
