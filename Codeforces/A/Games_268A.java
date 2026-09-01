d00fee227509375f587ee419e967711cc9e2259d
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-09-02 01:58:19 +0530

    Solved Problem 268A Games

---
+++ Codeforces/A/Games_268A.java
@@ -0,0 +1,25 @@
+package Codeforces.A;
+import java.util.*;
+public class Games_268A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] home = new int[n];
+        int[] guest = new int[n];
+        for (int i = 0; i < n; i++) {
+            home[i] = sc.nextInt();
+            guest[i] = sc.nextInt();
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && home[i] == guest[j]) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+    
+}
