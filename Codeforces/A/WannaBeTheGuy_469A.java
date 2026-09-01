f7ed30efc6c71baf33b11373f598ee7059931861
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-09-02 01:25:22 +0530

    Solves problem 469A WannaBeTheGuy

---
+++ Codeforces/A/WannaBeTheGuy_469A.java
@@ -0,0 +1,29 @@
+package Codeforces.A;
+import java.util.*;
+public class WannaBeTheGuy_469A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean[] passed = new boolean[n + 1];
+        int p = sc.nextInt();
+        for (int i = 0; i < p; i++) {
+            int level = sc.nextInt();
+            passed[level] = true;
+        }
+        int q = sc.nextInt();
+        for (int i = 0; i < q; i++) {
+            int level = sc.nextInt();
+            passed[level] = true;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!passed[i]) {
+                System.out.println("Oh, my keyboard!");
+                sc.close();
+                return;
+            }
+        }
+        System.out.println("I become the guy.");
+        sc.close();
+    }
+    
+}
