a5e28c30c47171c76eb48ec9515a76af79c4ab60
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-09-02 02:52:50 +0530

    Solved problem 443A AntonandLetters

---
+++ Codeforces/A/AntonandLetters_443A.java
@@ -0,0 +1,18 @@
+package Codeforces.A;
+import java.util.*;
+public class AntonandLetters_443A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        HashSet<Character> set = new HashSet<>();
+        for ( int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                set.add(ch);
+            }
+        }
+        System.out.println(set.size()); 
+        sc.close();
+    }
+    
+}
