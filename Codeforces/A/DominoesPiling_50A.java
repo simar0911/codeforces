e70303baf4308b632b29fbf0abe2b95b33f0ea26
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-09-08 01:28:58 +0530

    Solved Problem 50A

---
+++ Codeforces/A/DominoesPiling_50A.java
@@ -0,0 +1,13 @@
+package Codeforces.A;
+import java.util.*;
+public class DominoesPiling_50A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        System.out.println((M * N) / 2);
+        sc.close();
+    }
+}
+    
+
