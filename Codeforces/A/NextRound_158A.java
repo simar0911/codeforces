8095c828ec0ece20d624ed9bb6d7710ccbe4e8c3
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-09-08 23:13:42 +0530

    Solved problem 158A NextRound

---
+++ Codeforces/A/NextRound_158A.java
@@ -0,0 +1,31 @@
+package Codeforces.A;
+
+import java.util.*;
+
+public class NextRound_158A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] scores = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            scores[i] = sc.nextInt();
+        }
+
+        int cutoff = scores[k - 1];
+        int count = 0;
+
+        for (int score : scores) {
+            if (score >= cutoff && score > 0) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+} 
+    
+
