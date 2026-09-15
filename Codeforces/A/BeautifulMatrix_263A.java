b840102ddf770117253286ea585cf844d8735362
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-09-15 23:47:37 +0530

    Solved Problem 263A BeautifulMatrix

---
+++ Codeforces/A/BeautifulMatrix_263A.java
@@ -0,0 +1,26 @@
+package Codeforces.A;
+import java.util.*;
+public class BeautifulMatrix_263A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row = 0;
+        int col = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                int x = sc.nextInt();
+
+                if (x == 1) {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        int moves = Math.abs(row - 3) + Math.abs(col - 3);
+
+        System.out.println(moves);
+    }
+}
+    
