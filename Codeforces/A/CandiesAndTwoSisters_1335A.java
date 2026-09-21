8186602d9f96d56bd3680269cf074931402558aa
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-09-21 23:44:30 +0530

    Solved problem 1335A CandiesAndTwoSisters

---
+++ Codeforces/A/CandiesAndTwoSisters_1335A.java
@@ -0,0 +1,16 @@
+package Codeforces.A;
+import java.util.*;
+public class CandiesAndTwoSisters_1335A {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+
+            System.out.println((n - 1) / 2);
+        }
+    }
+}
+    
