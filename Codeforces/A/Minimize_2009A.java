584ed6ddc78214e6e5eb9730a6c370016306119c
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-09-23 22:30:40 +0530

    Solved problem 2009A Minimize!

---
+++ Codeforces/A/Minimize_2009A.java
@@ -0,0 +1,16 @@
+package Codeforces.A;
+import java.util.*;
+public class Minimize_2009A {
+       public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(b - a);
+        }
+    }
+}
