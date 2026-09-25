f76800e2db5b2d27f3150b830e8ea65491e3d006
simar0911 <simarkaur.9.11.2005@gmail.com>
2026-09-25 23:22:25 +0530

    Solved problem 281A WordCpitalization

---
+++ Codeforces/A/WordCapitaliization_281A.java
@@ -0,0 +1,14 @@
+package Codeforces.A;
+import java.util.*;
+public class WordCapitaliization_281A {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        s = Character.toUpperCase(s.charAt(0)) + s.substring(1);
+
+        System.out.println(s);
+    }
+}
